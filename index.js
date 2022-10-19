400883b6fd86a9b198d28a6b201663e6dc2dbae7
Falodun Oluwadamilola <dpaulzs11@gmail.com>
2022-10-19 15:21:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const username = prompt("Whats your name?: ");
+let range = 2;
+let trial = 1;
+let level = 1;
+let point = 0;
+
+console.log(`
+Welcome ${username}, Guess a wholenumber between 1 and ${range}, you have two trials
+level 1
+`)
+let guess = parseInt(prompt("Your Guess: "))
+
+const guessingGame = (range) => {
+  while (trial > 0) {
+    let random = Math.floor(Math.random() * range) + 1;
+    if (guess === random) {
+      range++
+      level++
+      trial = 2
+      point++
+      console.log(`Congrats you won ${username},you have ${point} point. Can you win level ${level}?, Guess a wholenumber between 1 and ${range}, you have two trials`)
+      guess = parseInt(prompt("Your Guess: "))
+      trial--
+    } else if (guess !== random) {
+      console.log(`Sorry try again, you have ${trial} trial remaining `)
+      guess = parseInt(prompt("Your Guess: "))
+      trial--
+      if (guess !== random) {
+        console.log(`Sorry ${username}, you lost`)
+        break
+      }
+    }
+  }
+}
+
+guessingGame(range);
